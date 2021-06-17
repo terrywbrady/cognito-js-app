5bd2a48dfc2aaee5737253225cf520d431b4eb4a
Terry Brady <terrywbrady@gmail.com>
2021-06-17 13:50:57 -0700

    Add mustache template

--- alb-intercept/lambda_function.rb
+++ alb-intercept/lambda_function.rb
@@ -1,27 +1,27 @@
 require 'json'
 require 'jwt'
+require 'mustache'
 
 def lambda_handler(event:, context:)
   token = event.fetch('headers',{}).fetch('x-amzn-oidc-accesstoken','')
   rxpath = %r[.*/(web/.*\.([^\.]*))$]
   path = event.fetch('path', '')
   mpath = rxpath.match(path)
+  return_page = !mpath.nil?
   reqresp = { 
       statusCode: 200, 
       headers: {'Content-Type' => 'text'},
       body: 'No action' 
   }
   if token.empty?
-    reqresp = error(401, "Unauthorized: No user token in header ")
+    reqresp = error(401, "Unauthorized: No user token in header ", return_page)
   else
     begin
       jtoken = JWT.decode(token, nil, false, { :algorithm => 'RS256' })
       #resp = jtoken
       username = "User token found for #{jtoken[0].fetch('username', '')}"
       groups = jtoken[0].fetch("cognito:groups", [])
-      if groups.length == 0
-        reqresp = error(401, "Unauthorized: No groups for user: #{username}")
-      elsif path == '/account/logout' 
+      if path == '/account/logout' 
         reqresp = { 
           statusCode: 303, 
           headers: {
@@ -34,7 +34,9 @@ def lambda_handler(event:, context:)
           statusCode: 303, 
           headers: {'Location' => ENV.fetch('LOGIN_URI', '.')}
         }
-      elsif mpath 
+      elsif groups.length == 0
+        reqresp = error(401, "Unauthorized: No groups for user: #{username}", return_page)
+      elsif return_page 
         reqresp = web_assets(mpath[1], mpath[2])
       else
         reqresp = { 
@@ -48,7 +50,7 @@ def lambda_handler(event:, context:)
         }
       end
     rescue => e
-      reqresp = error(500, "ERROR: #{e.message}")
+      reqresp = error(500, "ERROR: #{e.message}", return_page)
     end
   end
   reqresp
@@ -62,9 +64,9 @@ def content_type(ext)
 end
 
 def web_assets(path, ext)
-  return error(404, "File not found #{path}") unless File.file?(path)
+  return error(404, "File not found #{path}", false) unless File.file?(path)
   ctype = content_type(ext)
-  return error(404, "Unsupported content type #{ext}") unless ctype
+  return error(404, "Unsupported content type #{ext}", false) unless ctype
   { 
     statusCode: 200, 
     headers: {'Content-Type' => ctype},
@@ -72,10 +74,18 @@ def web_assets(path, ext)
   }
 end
 
-def error(status, message)
-  { 
-    statusCode: status, 
-    headers: {'Content-Type' => 'text'},
-    body: message 
-  }
+def error(status, message, return_page = false)
+  if status != 200 && return_page
+    { 
+      statusCode: 200, 
+      headers: {'Content-Type' => 'text/html'},
+      body: Mustache.render(File.open('template/error.html').read, message: message)
+    }  
+  else
+    { 
+      statusCode: status, 
+      headers: {'Content-Type' => 'text'},
+      body: message 
+    }
+  end
 end
